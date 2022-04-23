fdddae317778687c1fa52eab3b11bc33df26b551
ArdenButterfield <76457489+ArdenButterfield@users.noreply.github.com>
2022-04-23 13:44:28 -0700

    make it so the camera is more dynamic

--- CIS 410 final game/Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
+++ CIS 410 final game/Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+	// We store a reference to the car's transform, so we can get its position and rotation.
 	public Transform carTransform;
-	[Range(1, 10)]
-	public float followSpeed = 2;
-	[Range(1, 10)]
-	public float lookSpeed = 5;
-	public float distanceToCar = 5;
-	Vector3 initialCameraPosition;
-	Vector3 newCarPosition;
+	
+	// Do we want the camera to swing around, instead of being rigidly locked to the car?
+	public bool swingAround = true;
+
+	// How fast does the camera move, when attempting to get to the target position behind the car?
+	[Range (0.0f, 1.0f)]
+	public float cameraMoveSnap = 0.1f;
+
+	// How fast does the camera rotate, when attempting to get to the target rotation looking at the car?
+	[Range (0.0f, 1.0f)]
+	public float cameraTurnSnap = 0.1f;
+
+	// What is the target position for the camera to be relative to the car? i.e. 8 meters behind, 3 meters above.
 	Vector3 cameraDistanceFromCar = Vector3.back * 8 + Vector3.up * 3;
 
+
 	void Start(){
-		initialCameraPosition = gameObject.transform.position;
+		transform.position = carTransform.position + carTransform.rotation * cameraDistanceFromCar;
+		transform.rotation = carTransform.rotation;
 	}
 
 	void FixedUpdate()
 	{
-		transform.position = carTransform.position + carTransform.rotation * cameraDistanceFromCar;
-		transform.rotation = carTransform.rotation;
+		Vector3 targetPosition = carTransform.position + carTransform.rotation * cameraDistanceFromCar;
+		Quaternion targetRotation = carTransform.rotation;
+
+		if (swingAround) {
+			// If we want the camera to swing around, we don't snap immediately to the target position and rotation, but
+			// slowly move towards it.
+			transform.position = Vector3.Lerp(transform.position, targetPosition, cameraMoveSnap);
+			transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, cameraTurnSnap);
+		} else {
+			transform.position = targetPosition;
+			transform.rotation = carTransform.rotation;
+		}
+		
 	}
 
 }
