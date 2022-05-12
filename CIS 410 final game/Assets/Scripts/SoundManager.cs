7b89e0e3a60d99e9e02ec6dabdbf4d8a0df038c6
ArdenButterfield <76457489+ArdenButterfield@users.noreply.github.com>
2022-05-11 23:24:51 -0700

    laying the groundwork for sounds

---
+++ CIS 410 final game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioSource car1FXSource;
+    public AudioSource car2FXSource;
+
+    public AudioClip[] coinPickups;
+    public AudioClip musicLoop;
+    public AudioClip[] boostSounds;
+    public AudioClip[] driftSounds;
+    public AudioClip[] crashSounds;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
