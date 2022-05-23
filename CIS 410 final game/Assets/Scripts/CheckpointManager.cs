4d94358353c7b8eb06b4920596b08ddb071589fc
misccpu <supertriplejump950@gmail.com>
2022-05-22 20:05:08 -0700

    created start sign and began editing checkpointmanager to handle laps

--- CIS 410 final game/Assets/Scripts/CheckpointManager.cs
+++ CIS 410 final game/Assets/Scripts/CheckpointManager.cs
@@ -6,49 +6,76 @@ using UnityEngine.SceneManagement;
 
 public class CheckpointManager : MonoBehaviour
 {
-    private int car1points;
-    private int car2points;
-    private bool gameover;
+    public int required_laps = 3;
+    public int num_checkpoints_on_track = 5;
 
-    public int FinishLineCheckpoint;
+    // Car lap information. A lap only counts if all checkpoints exist in that car's list
+    private List<int> laps;
+    private List<List<int>> clearedCheckpoints;
+
     public GameObject WinPanel;
     public TextMeshProUGUI WinPanelText;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        car1points = 0;
-        car2points = 0;
-        WinPanel.SetActive(false);
-        gameover = false;
+        Debug.Log("In CheckpointManager Start()");
+        laps[1] = 1;
+        laps[2] = 1;
     }
 
     public void UpdateCheckpoints(int carnum, int checkpointnum)
     {
-        if (gameover) {
-            return;
-        }
+        // Add checkpoints to the car's as race continues.
+        clearedCheckpoints[carnum].Add(checkpointnum);
 
-        if ((carnum == 1) && (checkpointnum == car1points + 1)) {
-            car1points = checkpointnum;
-        } else if ((carnum == 2) && (checkpointnum == car2points + 1)) {
-            car2points = checkpointnum;
+        // If the car crosses the finish line...
+        if (checkpointnum == 0)
+        {
+            // ...end race if someone won!
+            if (laps[carnum] == required_laps)
+                EndRace(carnum);
+
+            // Otherwise, update lap if all checkpoints were triggered.
+            else if (LapWasCompleted(carnum))
+                    laps[carnum]++; Debug.Log("Car " + carnum + "on lap " + laps[carnum]);
         }
-        if (car1points == FinishLineCheckpoint) {
+    }
+
+    private bool LapWasCompleted(int carnum)
+    {
+        Debug.Log("In LapWasCompleted()");
+        // Assume failure
+        bool tickLap = false;
+        int checkpointCount = 0;
+
+        // Increment counter if checkpoint was in the list
+        for (int i = 1; i <= num_checkpoints_on_track; i++)
+            if (clearedCheckpoints[carnum].Contains(i))
+                checkpointCount++;
+
+        // If all checkpoints cleared, update return bool
+        if (checkpointCount == num_checkpoints_on_track)
+            tickLap = true;
+
+        return tickLap;
+    }
+
+    // Ends the race; declare the winner
+    private void EndRace(int winner)
+    {
+        Debug.Log("In EndRace()");
+        if (winner == 1)
             WinPanelText.text = ("Player 1 wins!");
-            gameover = true;
-            WinPanel.SetActive(true);
-        }
-        if (car2points == FinishLineCheckpoint) {
+        else if (winner == 2)
             WinPanelText.text = ("Player 2 wins!");
-            gameover = true;
-            WinPanel.SetActive(true);
-        }
+
+        WinPanel.SetActive(true);
     }
 
     public void LoadTitle()
     {
+        Debug.Log("In LoadTitle()");
         // Draws on this tutorial: https://www.youtube.com/watch?v=05OfmBIf5os
         SceneManager.LoadScene("title screen");
     }
