bda0e7f8c71afefb86c3dfd0238d1aede51be96c
ArdenButterfield <76457489+ArdenButterfield@users.noreply.github.com>
2022-06-03 10:28:41 -0700

    title screen instructions change

--- CIS 410 final game/Assets/Scripts/titlescreens/SingleplayerSelect.cs
+++ CIS 410 final game/Assets/Scripts/titlescreens/SingleplayerSelect.cs
@@ -9,11 +9,19 @@ using UnityEngine.UI;
 public class SingleplayerSelect : MonoBehaviour
 {
     public Toggle singleplayerToggle;
+    public TextMeshProUGUI controlInstructions;
+
+    public string singlePlayerInstructions = "move with WASD, boost is v (or right shift), drift is spacebar, HOLD r to respawn to checkpoint";
+    public string twoPlayerInstructions = "Player 1: move with WASD, boost is v (or right shift), drift is spacebar, HOLD r to respawn to checkpoint\nPlayer 2: move with PL;', boost is Return, drift is right shift, HOLD \\ to respawn to checkpoint";
 
     void Start() {
         print(PlayerPrefs.GetInt("singleplayer"));
         if (PlayerPrefs.GetInt("singleplayer") == 1) {
             singleplayerToggle.isOn = true;
+            controlInstructions.text = singlePlayerInstructions;
+        } else {
+            singleplayerToggle.isOn = false;
+            controlInstructions.text = twoPlayerInstructions;
         }
     }
 
@@ -24,9 +32,11 @@ public class SingleplayerSelect : MonoBehaviour
         if (singleplayerToggle.isOn) {
             print("On");
             PlayerPrefs.SetInt("singleplayer", 1);
+            controlInstructions.text = singlePlayerInstructions;
         } else {
             print("Off");
             PlayerPrefs.SetInt("singleplayer", 0);
+            controlInstructions.text = twoPlayerInstructions;
         }
     }
 }
